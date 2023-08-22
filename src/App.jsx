290e86184a2319546cfa364798a33658db9d28fa
Ainan <ainanhassan123@gmail.com>
2023-08-22 21:18:51 +0300

    random display of cards done now gameover modal

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import { useState } from 'react';
 import Card from './Components/Card';
 import './App.css';
 import Score from './Components/Score';
-const Shuffle = require('react-shuffle');
+
+
 
 
 import image1 from "./assets/image1.jpg";
@@ -56,13 +57,17 @@ function execute(param){
      <div> <Score score={score} /> </div>
     <div className="cardcontainer">
 
-      <Shuffle>
-      {Cards.map((item) => {
-        return     <Card key={item.index} title={item.title} image={item.Imgsrc} onclick={() => execute(item.index)} />  
+      
+      {Cards
+    .map((v) => ({ val: v, key: Math.random() })) 
+    .sort((a, b) => a.key - b.key)                
+    .map((o) => o.val)
+    .map((item) => {
+        return     <Card key={item.index}  image={item.Imgsrc} onclick={() => execute(item.index)} />  
         
 
       } )}
-      </Shuffle>
+     
       
      
       
---
+++ src/Components/GameOver.jsx
@@ -0,0 +1,9 @@
+export default function GameOver() {
+    return(
+   <div className="modal">
+    <h1>Game Over </h1>
+
+     <button className="play-again"> Play again</button>
+   </div>
+    )
+}
